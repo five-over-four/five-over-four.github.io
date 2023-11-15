9665923887d76cc75a2b80b7bdf94ec4f86a4003
not-legato <tommirossidude@gmail.com>
2023-11-15 14:05:24 +0200

    woops left a debugging log in there

--- scripts/dynamic_topmargin.js
+++ scripts/dynamic_topmargin.js
@@ -3,7 +3,6 @@ function resizeTopMargin() {
     const bodyh = document.body.clientHeight;
     const toph = document.getElementById("main-container").clientHeight;
     document.getElementById("main-container").style.marginTop = `${0.2 * (bodyh - toph)}px`;
-    console.log(document.getElementById("main-container").style.cssText);
 }
 
 window.addEventListener('load', resizeTopMargin);
