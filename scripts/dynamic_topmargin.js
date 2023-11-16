02a1c7e5b8b5f244145222484cdcac44203551f6
not-legato <tommirossidude@gmail.com>
2023-11-16 17:18:23 +0200

    big styling improvements for the floating elements like updates and contact etc.

--- scripts/dynamic_topmargin.js
+++ scripts/dynamic_topmargin.js
@@ -1,8 +1,20 @@
-// resize top margin to percentage% the empty space below.
+// resize top margin to 30% the empty space below.
 function resizeTopMargin() {
     const bodyh = document.body.clientHeight;
     const toph = document.getElementById("main-container").clientHeight;
-    document.getElementById("main-container").style.marginTop = `${0.2 * (bodyh - toph)}px`;
+
+    // desktop.
+    if (screen && screen.width > 900) {
+        document.getElementById("main-container").style.marginTop = `${0.25 * (bodyh - toph)}px`;
+    }
+    // tall mobile.
+    else if (screen && screen.width <= 900 && bodyh > 700) {
+        document.getElementById("main-container").style.marginTop = `${0.02 * (bodyh - toph)}em`;
+    }
+    // short mobile.
+    else {
+        document.getElementById("main-container").style.marginTop = `${0.03 * (bodyh - toph)}em`;
+    }
 }
 
 window.addEventListener('load', resizeTopMargin);
